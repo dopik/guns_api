99145fc0fcbd60aca4122f0f5b314173507b2f25
dopik <33575686+dopik@users.noreply.github.com>
2018-06-26 11:36:58 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ local STANDARD_MAGAZINE = 8
 local STANDARD_AMMUNITION = nil
 
 
-local hex = {0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e"}
+local hex = {0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"}
 
 
 local curtime = os.time()
